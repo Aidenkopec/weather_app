0f1a3a463c25f3f787f18c6aff8b4cffbd064490
Aidenkopec <97480728+Aidenkopec@users.noreply.github.com>
2023-05-07 22:13:05 -0600

    Updated weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Weather App
+ *
+ * This is a simple React application that allows users to search for the current weather in any city.
+ * The app fetches weather data from an external API and displays the results.
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Import required modules
+import React, {useState} from 'react';
+
+// Import custom fetchWeather function
+import {fetchWeather} from './api/fetchWeather';
+// Import styling
+import './App.css';
+// import backgroundImage from './backgroundWeather.png';
+
+/**
+ * App component
+ *
+ * The main component of the Weather App. It handles user input, fetching weather data, and rendering
+ * the results.
+ */
+const App = () => {
+    // State for search query and weather data
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    /**
+     * search function
+     *
+     * Handles fetching weather data for a given city. Triggered when the user presses the Enter key
+     * or clicks the search button.
+     */
+    const search = async () => {
+        // Fetch weather data for the given query
+        const data = await fetchWeather(query);
+
+        // Update state with fetched weather data and clear query
+        setWeather(data);
+        setQuery('');
+    };
+
+    // Render main app components
+    return (
+        <div className="main-container">
+            <div className="search-wrapper">
+                <input
+                    type="text"
+                    className="search"
+                    placeholder="Search for a city..."
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onKeyPress={(e) => e.key === 'Enter' && search()}
+                />
+                <button onClick={search} className="search-btn">Search</button>
+            </div>
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img
+                            className="city-icon"
+                            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                            alt={weather.weather[0].description}
+                        />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
